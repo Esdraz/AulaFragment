67950b873a9a72947273806fd36e2d92da43ec57
Francisco <fsousa.jpa@gmail.com>
2024-09-30 15:01:18 -0300

    add fragments chamadas e conversas

--- app/src/main/java/com/esdraz/aulafragment/MainActivity.kt
+++ app/src/main/java/com/esdraz/aulafragment/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.esdraz.aulafragment
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.esdraz.aulafragment.fragments.ChamadasFragment
+import com.esdraz.aulafragment.fragments.ConversasFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnConversas: Button
+    private lateinit var btnChamadas: Button
+    private lateinit var btnRemoverFragment: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +23,44 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnConversas = findViewById(R.id.btn_conversas)
+        btnChamadas = findViewById(R.id.btn_chamadas)
+        btnRemoverFragment = findViewById(R.id.btn_remover_fragment)
+
+//        val fragmentManager = supportFragmentManager.beginTransaction()
+//        fragmentManager.add(R.id.fragment_conteudo, ConversasFragment())
+//        fragmentManager.commit()
+
+        //instanciando o conversasFragment para poder remover com o .remove no btnChamadas
+        val conversasFragment = ConversasFragment()
+        val chamadasFragment = ChamadasFragment()
+
+        btnConversas.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+//                .replace(R.id.fragment_conteudo, ConversasFragment())
+                .replace(R.id.fragment_conteudo, conversasFragment)
+                .commit()
+        }
+
+        btnChamadas.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+//                .replace(R.id.fragment_conteudo, ChamadasFragment())
+                .replace(R.id.fragment_conteudo, chamadasFragment)
+//                .remove(conversasFragment)
+                .commit()
+        }
+
+        //.remove remove a instância do fragment designado
+        btnRemoverFragment.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .remove(conversasFragment)
+                .remove(chamadasFragment)
+                .commit()
         }
     }
 }
---
+++ app/src/main/java/com/esdraz/aulafragment/fragments/ConversasFragment.kt
@@ -0,0 +1,19 @@
+package com.esdraz.aulafragment.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.esdraz.aulafragment.R
+
+class ConversasFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_conversas, container, false)
+    }
+}
